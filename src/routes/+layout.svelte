530a0aba07d98d8efe147217f41590085df36905
endigo9740 <gundamx9740@gmail.com>
2022-08-23 11:12:57 -0500

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import "../app.postcss";
+</script>
+
+<svelte:head>
+  <title>Sveltekit Vercel Test</title>
+</svelte:head>
+
+<nav class="flex space-x-4 mb-4">
+  <a href="/">Homepage</a>
+  <a href="/about">About</a>
+</nav>
+<slot />
